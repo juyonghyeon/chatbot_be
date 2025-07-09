ad672c6b03c084b5131588ef098f2d5446cc53af
juyonghyeon <jyh928578@gmail.com>
2025-07-09 17:16:52 +0900

    챗봇 작업 중

---
+++ src/main/java/org/koreait/chatbot/services/ChatService.java
@@ -0,0 +1,18 @@
+package org.koreait.chatbot.services;
+
+import lombok.RequiredArgsConstructor;
+import org.koreait.global.configs.PythonProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Lazy
+@Service
+@RequiredArgsConstructor
+@EnableConfigurationProperties(PythonProperties.class)
+public class ChatService {
+    private final PythonProperties properties;
+    private final WebMvcConfigurer ctx;
+
+}
